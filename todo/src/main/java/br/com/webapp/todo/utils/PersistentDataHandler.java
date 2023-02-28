679ccb3521c114f0cfbc5beed9797ee8f492a5ca
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 07:55:27 -0300

    feat - adicionando classe para lidar com permenancia de dados na web com cookie
    adicionando classe para lidar com permenancia de dados na web com cookie

---
+++ todo/src/main/java/br/com/webapp/todo/utils/PersistentDataHandler.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.webapp.todo.utils;
+
+import java.util.*;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jpmgo
+ */
+@ApplicationScoped
+public class PersistentDataHandler {
+}
