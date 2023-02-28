dbe89589c4d94d8ff693c94c381ee415ce38b16a
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 18:25:21 -0300

    feat - adicionando metodos global de userDetailsService para auth jwt

--- api/src/main/java/br/com/todo/api/config/ApplicationConfig.java
+++ api/src/main/java/br/com/todo/api/config/ApplicationConfig.java
@@ -16,4 +16,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+
+  private final IUserRepository repository;
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username ->
+      repository
+        .findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
 }
