aa4f1c8182cd45d8d44bf90ec47026f4aa9b3bf8
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 07:50:38 -0300

    feat - adicionando classe para lidar com comunicação web
    adicionando classe para lidar com comunicação web

---
+++ todo/src/main/java/br/com/webapp/todo/utils/WebComunication.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.webapp.todo.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author jpmgo
+ */
+public class WebComunication {
+}
