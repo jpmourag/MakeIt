fc96bf7e2c7d975dc104481bcab229090a7db6c7
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 19:45:12 -0300

    feat - adicionando dto
    dto para atualização de um user

---
+++ api/src/main/java/br/com/todo/api/modules/user/dto/UpdateUserDto.java
@@ -0,0 +1,16 @@
+package br.com.todo.api.modules.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserDto {
+    @NotBlank(message = "Name is required")
+    private String name;
+}
