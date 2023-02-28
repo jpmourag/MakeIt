0911afa0517c000e53777f537fcf4373cee6aab5
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 20:02:22 -0300

    feat - folder class controller
    criação da classe controller de folder

---
+++ api/src/main/java/br/com/todo/api/modules/folder/FolderController.java
@@ -0,0 +1,28 @@
+package br.com.todo.api.modules.folder;
+
+import br.com.todo.api.exceptions.CustomResponseEntityExceptionHandler;
+import br.com.todo.api.exceptions.throwables.entity.EntityException;
+import br.com.todo.api.exceptions.throwables.entity.FolderException;
+import br.com.todo.api.exceptions.throwables.global.UnmappedErrorException;
+import br.com.todo.api.global.dto.ResponseBaseDto;
+import br.com.todo.api.modules.folder.dto.CreateFolderDto;
+import br.com.todo.api.modules.folder.dto.UpdateFolderDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/folder")
+public class FolderController {
+    @Autowired
+    private FolderService folderService;
+}
