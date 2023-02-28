69e0d45fe586e64fc98cf64fd8dedc24c62b9768
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 20:21:29 -0300

    feat - task user controller
    criação da classe controller de user

---
+++ api/src/main/java/br/com/todo/api/modules/user/UserController.java
@@ -0,0 +1,31 @@
+package br.com.todo.api.modules.user;
+
+import br.com.todo.api.exceptions.CustomResponseEntityExceptionHandler;
+import br.com.todo.api.exceptions.throwables.entity.EntityException;
+import br.com.todo.api.exceptions.throwables.entity.UserException;
+import br.com.todo.api.exceptions.throwables.global.UnmappedErrorException;
+import br.com.todo.api.global.dto.ResponseBaseDto;
+import br.com.todo.api.modules.user.dto.AuthRequestDto;
+import br.com.todo.api.modules.user.dto.CreateUserDto;
+import br.com.todo.api.modules.user.dto.UpdateUserDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
