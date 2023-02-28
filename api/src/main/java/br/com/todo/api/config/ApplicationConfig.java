eb4a09daa971022a2557679c46f1ac8e304efaab
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 18:19:10 -0300

    criando classe de configuração global do app

---
+++ api/src/main/java/br/com/todo/api/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package br.com.todo.api.config;
+
+import br.com.todo.api.repositories.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+}
