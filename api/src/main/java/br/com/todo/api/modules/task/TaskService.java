9ffad3303490ab8dd669f4717af925c2b3c23670
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 20:38:55 -0300

    feat - task class service
    criança classe para controlar a regra de negocio

---
+++ api/src/main/java/br/com/todo/api/modules/task/TaskService.java
@@ -0,0 +1,37 @@
+package br.com.todo.api.modules.task;
+
+import br.com.todo.api.entities.Task;
+import br.com.todo.api.exceptions.throwables.entity.FolderException;
+import br.com.todo.api.global.dto.PaginationBaseResponseDto;
+import br.com.todo.api.global.dto.ResponseBaseDto;
+import br.com.todo.api.modules.folder.FolderService;
+import br.com.todo.api.modules.task.dto.CreateTaskDto;
+import br.com.todo.api.modules.task.dto.TaskPaginationDto;
+import br.com.todo.api.modules.task.dto.UpdateTaskDto;
+import br.com.todo.api.modules.user.UserService;
+import br.com.todo.api.repositories.ITaskRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+// TODO
+// # criar/pegar/deletar/atualizar task
+// pegar quantidade total de task q o folder tem
+// pegar range de task pelo id do folder - pagination
+
+@Service
+public class TaskService {
+    @Autowired
+    private ITaskRepository taskRepository;
+    @Autowired
+    private FolderService folderService;
+    @Autowired
+    private UserService userService;
+}
