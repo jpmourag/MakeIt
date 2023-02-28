49d6a79e3d57a30b066e23a2986a9b82c70dcee6
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 08:01:19 -0300

    feat - classe criada para portar funções genericas manipulam a view de alguma forma
    classe criada para portar funções genericas manipulam a view de alguma forma

---
+++ todo/src/main/java/br/com/webapp/todo/utils/ExtraForView.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.webapp.todo.utils;
+
+import java.util.Random;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author jpmgo
+ */
+public class ExtraForView {
+}
