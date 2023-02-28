00c5caaa4298bd7fa2d4fd096bd70b49250be0df
JPMouraG <jp.mouragoes@gmail.com>
2023-02-28 19:44:09 -0300

    feat - adicionando dto
    dto para retornar valores apos o user ser autentificado

---
+++ api/src/main/java/br/com/todo/api/modules/user/dto/AuthResponseDto.java
@@ -0,0 +1,14 @@
+package br.com.todo.api.modules.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDto {
+  private String token;
+}
